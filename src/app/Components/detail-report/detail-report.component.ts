76e53de91f212ac04a34f0cb37e04b0d6358851f
danisamaniego370 <danisamaniego370@gmail.com>
2025-03-12 15:37:11 +0000

    First commit

---
+++ src/app/Components/detail-report/detail-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-report',
+  imports: [],
+  templateUrl: './detail-report.component.html',
+  styleUrl: './detail-report.component.css'
+})
+export class DetailReportComponent {
+
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CameraComponent } from './Components/camera/camera.component';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CameraComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'myapp';
+}
